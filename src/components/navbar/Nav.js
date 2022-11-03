315ed27cc24ab789d1958c8168bcaadb1c6a848f
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-11-03 18:13:19 +0530

    react app created

---
+++ src/components/navbar/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './nav.css';
+
+const Nav = () => {
+  return (
+    <div className='nav-cnt'>
+      <h2 className='nav-heading'>Happy Coffee</h2>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/pages/mainpage/MainPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <h1>Order your coffee</h1>
+          <button>Oder</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
